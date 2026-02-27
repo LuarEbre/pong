db7025086f6e4885d64b0c7c4f233e05983920a5
LuarEbre <raule0822@gmail.com>
2026-02-27 12:45:08 +0100

    Change app title to "pong"

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 -- configures the game before main runs
 function love.conf(t)
-    t.window.title = "Hello World"
+    t.window.title = "pong"
     t.window.width = 1000
     t.window.height = 800
     t.window.resizable = false
